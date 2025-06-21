7d28a8df7981b276875e3d79e09013cf51e233a4
pugazh2005 <142927727+pugazh2005@users.noreply.github.com>
2025-06-21 22:28:21 +0530

    Password_hidden

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/Library";
     private static final String USER = "root";
-    private static final String PASSWORD = "pugazh@2005"; 
+    private static final String PASSWORD = "********"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
